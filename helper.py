b0dbed318e573b09a40a28af08b2e6c8835a3bf2
Pearl <w2crosss@gmail.com>
2023-07-26 12:24:12 +0900

    update

--- helper.py
+++ helper.py
@@ -384,96 +384,6 @@ def spearman_r(df):
 
     return rdf
 
-
-class OlsResult:
-    def __init__(self):
-        self.__model = None
-        self.__fit = None
-        self.__summary = None
-        self.__table = None
-        self.__result = None
-        self.__goodness = None
-        self.__varstr = None
-
-    @property
-    def model(self):
-        """
-        분석모델
-        """
-        return self.__model
-
-    @model.setter
-    def model(self, value):
-        self.__model = value
-
-    @property
-    def fit(self):
-        """
-        분석결과 객체
-        """
-        return self.__fit
-
-    @fit.setter
-    def fit(self, value):
-        self.__fit = value
-
-    @property
-    def summary(self):
-        """
-        분석결과 요약 보고
-        """
-        return self.__summary
-
-    @summary.setter
-    def summary(self, value):
-        self.__summary = value
-
-    @property
-    def table(self):
-        """
-        결과표
-        """
-        return self.__table
-
-    @table.setter
-    def table(self, value):
-        self.__table = value
-
-    @property
-    def result(self):
-        """
-        결과표 부가 설명
-        """
-        return self.__result
-
-    @result.setter
-    def result(self, value):
-        self.__result = value
-
-    @property
-    def goodness(self):
-        """
-        모형 적합도 보고
-        """
-        return self.__goodness
-
-    @goodness.setter
-    def goodness(self, value):
-        self.__goodness = value
-
-    @property
-    def varstr(self):
-        """
-        독립변수 보고
-        """
-        return self.__varstr
-
-    @varstr.setter
-    def varstr(self, value):
-        self.__varstr = value
-
-
-
 def ext_ols(data, y, x):
     """
     회귀분석을 수해한다.
@@ -594,6 +504,95 @@ def ext_ols(data, y, x):
 
     # 리턴할
     return (model, fit, summary, table, result, goodness, varstr)
+
+
+
+class OlsResult:
+    def __init__(self):
+        self._model = None
+        self._fit = None
+        self._summary = None
+        self._table = None
+        self._result = None
+        self._goodness = None
+        self._varstr = None
+
+    @property
+    def model(self):
+        """
+        분석모델
+        """
+        return self._model
+
+    @model.setter
+    def model(self, value):
+        self._model = value
+
+    @property
+    def fit(self):
+        """
+        분석결과 객체
+        """
+        return self._fit
+
+    @fit.setter
+    def fit(self, value):
+        self._fit = value
+
+    @property
+    def summary(self):
+        """
+        분석결과 요약 보고
+        """
+        return self._summary
+
+    @summary.setter
+    def summary(self, value):
+        self._summary = value
+
+    @property
+    def table(self):
+        """
+        결과표
+        """
+        return self._table
+
+    @table.setter
+    def table(self, value):
+        self._table = value
+
+    @property
+    def result(self):
+        """
+        결과표 부가 설명
+        """
+        return self._result
+
+    @result.setter
+    def result(self, value):
+        self._result = value
+
+    @property
+    def goodness(self):
+        """
+        모형 적합도 보고
+        """
+        return self._goodness
+
+    @goodness.setter
+    def goodness(self, value):
+        self._goodness = value
+
+    @property
+    def varstr(self):
+        """
+        독립변수 보고
+        """
+        return self._varstr
+
+    @varstr.setter
+    def varstr(self, value):
+        self._varstr = value
 
 
 def my_ols(data, y, x):
